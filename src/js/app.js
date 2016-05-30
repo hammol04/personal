7f4cebddc1055ecd1a2e5fafa483e4153b256b6b
Laurence Hammond <laurence.hammond@bbc.co.uk>
2016-05-30 18:02:51 +0100

    Progress

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Page from "./components/Page";
 
-const app = document.getElementById('app');
-ReactDOM.render(<Page/>, app);
+if (typeof window !== undefined) {
+  const app = document.getElementById('app');
+  ReactDOM.render(<Page/>, app);
+}
