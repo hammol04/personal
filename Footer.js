bba76d7c6026d6150f7c593e686bd2be6c618cb3
Laurence Hammond <laurence.hammond@bbc.co.uk>
2016-05-30 18:34:44 +0100

    Deployment

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Copyright from "./Footer/Copyright";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer class="footer spacing spacing--horizontal">
+        <Copyright text="Made by Laurence Hammond. Source code on github. /hammol04" />
+      </footer>
+    );
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Page from "./components/Page";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Page/>, app);
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Page}>
+    <IndexRoute component={Page}></IndexRoute>
+    </Route>
+  </Router>,
+app);
