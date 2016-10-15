148ff54a8379f3bd850375af21096bfdc7e16f83
Laurence Hammond <laurence.hammond@bbc.co.uk>
2016-10-15 18:27:30 +0100

    Updating cv link

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -8,7 +8,7 @@ export default class Navigation extends React.Component {
         <Link title="Photography" link="http://ham-made.format.com"/>
         <Link title="LinkedIn" link="https://www.linkedin.com/in/laurence-hammond-162648a5"/>
         <Link title="GitHub" link="https://github.com/hammol04"/>
-        <Link title="CV" link="https://www.dropbox.com/s/c5l2is3e0q8vxep/CV%20-%20Laurence%20James%20Hammond.pdf?dl=0"/>
+        <Link title="CV" link="https://www.dropbox.com/s/qu4kjtm5t8bfjo8/CV%20-%20Laurence%20James%20Hammond.pdf?dl=0"/>
         <Link title="Email" link="mailto:247hammond@live.com"/>
       </nav>
     );
